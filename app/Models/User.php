a3e6b3edf5ac431a26624183b45555fdcf03af4b
hamzaelk04 <hamzakabraoui07@gmail.com>
2026-04-01 17:22:17 +0100

    create the role model and set the relation between them

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'firstname',
+        'lastname',
         'email',
         'password',
+        'role_id'
     ];
 
     /**
@@ -44,5 +46,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function Role()
+    {
+        return $this->belongsTo(Role::class);
     }
 }
