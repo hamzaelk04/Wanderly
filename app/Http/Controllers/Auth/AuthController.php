5a1d8662e2fedb4776ba007f56722e00bf4cfcc3
hamzaelk04 <hamzakabraoui07@gmail.com>
2026-04-01 20:57:27 +0100

    register done

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -14,13 +17,24 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        $request->validate([
+        $validated = $request->validate([
             'firstname' => 'required|string|max:20',
             'lastname' => 'required|string|max:20',
             'email' => 'required|email|unique:users,email',
             'password' => 'required|string|min:8|confirmed',
         ]);
-        
+
+        $roleName = $request->has('organizer') ? 'organizer' : 'client';
+        $roleId = Role::where('name', $roleName)->first();
+
+        User::create([
+            'firstname' => $validated['firstname'],
+            'lastname' => $validated['lastname'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+            'role_id' => $roleId->id,
+        ]);
+
         return view('test');
     }
 
@@ -31,6 +45,6 @@ class AuthController extends Controller
 
     public function logout()
     {
-        
+
     }
 }
