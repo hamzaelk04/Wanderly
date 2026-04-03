e345bbbe16888cfc2d26153771c7875ba8229e96
hamzaelk04 <hamzakabraoui07@gmail.com>
2026-04-03 22:10:55 +0100

    add the title and description for the main page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,17 @@
             <img class="w-full h-full object-cover" src="{{ asset('images/home.png') }}" alt="">
             <div class="absolute inset-0 bg-linear-to-b from-black/40 via-transparent to-background"></div>
         </div>
+        <div class="relative z-10 max-w-5xl w-full text-center space-y-8">
+            <div class="space-y-4">
+                <h1 class="text-5xl md:text-7xl font-extrabold font-headline text-white tracking-tight">
+                    Organiser Votre Prochain <br/>
+                    <span class="text-secondary-container">Grande Aventure</span>
+                </h1>
+                <p class="text-lg md:text-xl text-white/90 font-body max-w-2xl mx-auto">
+                    Découvrez des événements culturels exclusifs, des monuments cachés et des rassemblements artistiques adaptés à votre curiosité.
+                </p>
+            </div>
+        </div>
     </section>
 </main>
 @endsection
