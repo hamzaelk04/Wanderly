5b60229a0aabc2b6f5af6bcf696f9c2323ebd8d6
hamzaelk04 <hamzakabraoui07@gmail.com>
2026-04-09 17:28:58 +0100

    update the sidebar

--- resources/views/dasboard-partials/sidebar.blade.php
+++ resources/views/dasboard-partials/sidebar.blade.php
@@ -24,8 +24,8 @@
         </a>
         <a class="flex items-center gap-3 p-3 text-slate-500 hover:bg-slate-200/50 hover:translate-x-1 font-['Plus_Jakarta_Sans'] text-sm font-medium transition-all duration-200 ease-in-out"
             href="#">
-            <span class="material-symbols-outlined" data-icon="leaderboard">leaderboard</span>
-            <span>Analytics</span>
+            <span class="material-symbols-outlined" data-icon="add_circle">add_circle</span>
+            <span>Create Event</span>
         </a>
         <a class="flex items-center gap-3 p-3 text-slate-500 hover:bg-slate-200/50 hover:translate-x-1 font-['Plus_Jakarta_Sans'] text-sm font-medium transition-all duration-200 ease-in-out"
             href="#">
