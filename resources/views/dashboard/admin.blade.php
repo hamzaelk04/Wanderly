c33071de9f3d1a369e1f7bd1cc2cb21997ed0bfd
hamzaelk04 <hamzakabraoui07@gmail.com>
2026-04-23 09:42:49 +0100

    fixing the UI error in the admin.blade.php

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -79,14 +79,14 @@
                 <div class="mt-6 space-y-3">
                     <div class="flex justify-between items-center">
                         <div class="flex items-center gap-2">
-                            <span class="w-3 h-3 rounded-full bg-[#0077b6]"></span>
+                            <span class="w-3 h-3 rounded-full bg-primary-container"></span>
                             <span class="text-sm font-medium text-slate-600">Ticket Revenue</span>
                         </div>
                         <span class="text-sm font-bold">$184,200</span>
                     </div>
                     <div class="flex justify-between items-center">
                         <div class="flex items-center gap-2">
-                            <span class="w-3 h-3 rounded-full bg-[#f9ad00]"></span>
+                            <span class="w-3 h-3 rounded-full bg-secondary"></span>
                             <span class="text-sm font-medium text-slate-600">Profit Revenue</span>
                         </div>
                         <span class="text-sm font-bold">$61,400</span>
@@ -113,7 +113,7 @@
                 <img class="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
                     data-alt="vibrant nightlife scene at a modern music festival with purple and blue stage lights and silhouette of a cheering crowd"
                     src="https://lh3.googleusercontent.com/aida-public/AB6AXuCYU0MvQDbHWHmCHqD3AkPhsS8P8HCj_t-WkWjKORQwW--5hGiYna1lT-H5r4l1-9ShiCFAWfcMrDkfL5Q_CeG_S88x6V9KQfINUCyH9Zgfo5fGBbd4tgdlCBGQ_Bvk6M9MoWzzQyiCjIA0XQEuF4t6gKIbKXwg1dLODUg-X3y9ecIvW4UgtHgKYf-hoHC_01h7e5DYgnjDWbGGYfUn6ly3iMkF2tjAZ0smKTpkNrR8wAQGSwxrIFEr3ZFb5ATnVv_fl8AVTp6ZMmM" />
-                <div class="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent flex flex-col justify-end p-5">
+                <div class="absolute inset-0 bg-linear-to-t from-black/80 to-transparent flex flex-col justify-end p-5">
                     <span
                         class="bg-primary/80 backdrop-blur-md text-white text-[10px] uppercase font-bold tracking-widest px-2 py-1 rounded w-max mb-2">Top
                         Performer</span>
@@ -124,7 +124,7 @@
                 <img class="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
                     data-alt="elegant speakeasy cocktail bar with dark wood textures and amber lighting focusing on a crystal glass drink"
                     src="https://lh3.googleusercontent.com/aida-public/AB6AXuDLTZvhdkh3EWONra3XOoeyStoOx7MntStvTgYnmQIsof2HMfqqbSRNdfO_pwyvTk-5HzkoP49WIYlTYkIAPLPDfJ53pj7tgVFwXFPTC-J3B2zJZEGm3zcnwvZS_Y5gqJOV7wOTXFNjxzE8UcVGZ_NT3n_nlxTkbluQzUKjWibmV4JPSstykaErBt8jEhflv-EwD89uqEti7S10097ixnVsU5EzwfewIFticJtH0Y_wieMLKyiHvvL3e4axVK-03nuv8Y92Xhl4Zjg" />
-                <div class="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent flex flex-col justify-end p-5">
+                <div class="absolute inset-0 bg-linear-to-t from-black/80 to-transparent flex flex-col justify-end p-5">
                     <h4 class="text-white font-bold text-sm">The Hidden Cask</h4>
                 </div>
             </div>
@@ -132,7 +132,7 @@
                 <img class="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
                     data-alt="zen minimalist art gallery space with white walls and a large contemporary painting under focused spotlight"
                     src="https://lh3.googleusercontent.com/aida-public/AB6AXuAUuuw7YzM73nD_M9y-oDgt6DiI-FV4OviapoHasenuIGbboB-uAVs5BeKvfCn2GKhMG75-vQSXPcdMq5vwESnTUVJUUC7HwN-Dujjl2McMjTRbHEmruKnesdYgZog2mBcNYlUclAO7YLDkbx7qGdqrV6K9znI9c6j2YiS6si72cmHzDXvOH2iazl5q5t3T50NQbubNkgVXClRvyzBUToSa06b4tvRQw578YQELvbD14zuMqAqxkncSH9RKEyYBuHigPuKQWoB6aa0" />
-                <div class="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent flex flex-col justify-end p-5">
+                <div class="absolute inset-0 bg-linear-to-t from-black/80 to-transparent flex flex-col justify-end p-5">
                     <h4 class="text-white font-bold text-sm">Elysium Gallery Tour</h4>
                 </div>
             </div>
